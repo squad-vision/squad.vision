20451c87c65a2ac023f4f31536b07189d8dd83c9
tate <tateflyer1@gmail.com>
2021-02-05 17:49:06 -0500

    feat: contract state & active wallets

--- analytics/generateContract.js
+++ analytics/generateContract.js
@@ -1,29 +1,58 @@
 const Arweave = require("arweave");
 const fs = require("fs");
+const fetch = require("node-fetch");
+const cliProgress = require("cli-progress");
+const { parse } = require("path");
 
+const prog = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 const client = new Arweave({
   host: "arweave.net",
   port: 443,
   protocol: "https",
 });
 
-function getWallets() {
-  const data = fs.readFileSync("./02.03.2021-20.11/wallets.json");
-  return JSON.parse(data);
-}
+async function getWallets() {
+  // const data = fs.readFileSync("./02.05.2021/wallets.json");
+  
+  // Counting variables
+  let totalSQUAD = 0;
+  let addressCount = 0;
+  
+  // Pull all wallets
+  let allBalances = {};
+  const raw = await fetch("https://arweave.net/wallet_list");
+  let res = await raw.json();
+  prog.start(res.length, 0);
 
-function normalizeBalances(walletArray) {
-  let updatedBalances = {};
-  for (const [address, value] of Object.entries(walletArray)) {
-    const roundedBalance = Math.round(value);
-    if (roundedBalance > 0) {
-      updatedBalances = {
-        ...updatedBalances,
-        [address]: roundedBalance
-      };
+  // Iterate through wallets
+  for (let i = 0; i < res.length; i++) {
+    // If wallet is wallet
+    if (res[i].address.length > 40) {
+      res[i].balance = parseFloat(res[i].balance);
+      res[i].balance = client.ar.winstonToAr(res[i].balance);
+
+      // If 0.1 < wallet balance < 1 then round up
+      if (res[i].balance >= 0.1 && res[i].balance <= 1) res[i].balance = 1;
+      res[i].balance = Math.round(res[i].balance);
+      // If the wallet has a balance
+      if (res[i].balance > 0) {
+        // Add to balances object
+        allBalances = {
+          ...allBalances,
+          [res[i].address]: res[i].balance
+        };
+
+        // Increment the counters
+        totalSQUAD += res[i].balance
+        addressCount++;
+      }
     }
+    prog.update(i + 1);
   }
-  return updatedBalances;
+
+  console.log("Total $QUAD", totalSQUAD);
+  console.log("Number of addresses", addressCount);
+  return allBalances;
 }
 
 function generateInitialState(balances) {
@@ -49,6 +78,10 @@ function generateInitialState(balances) {
   }
 }
 
-const wallets = getWallets();
-const trueBalances = normalizeBalances(wallets);
-console.log(generateInitialState(trueBalances));
+(async () => {
+  console.log("Fetching wallets");
+  const wallets = await getWallets();
+  const contract = generateInitialState(wallets);
+  fs.writeFileSync("./state.json", JSON.stringify(contract, undefined, 2));
+  console.log(`\nOutput written to file: "./state.json"`);
+})();
